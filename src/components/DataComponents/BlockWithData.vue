135fedb2695524fb0771aba55d43872b440da689
1mk0v <potapchuk01@mail.ru>
2024-03-06 16:18:46 +0300

    New data component

---
+++ src/components/DataComponents/BlockWithData.vue
@@ -0,0 +1,73 @@
+<template>
+  <div 
+    class="key-container" 
+    @click="eventHandler" 
+    :style="{
+      flex: (flexAuto) ? '1 1 auto' : 'none',
+      border: (isButton) ? '1px solid #fff' : 'none',
+      padding: blockPaddingValue
+    }">
+    <div
+      :style="{
+        alignSelf: textAlign,
+        fontSize: textFontSize,
+        fontWeight: textFontWeight
+      }">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      defaultFontSize: '18px',
+      defaultFontWeight: '400',
+      defaultBlockPadding: '5px 10px'
+    }
+  },
+  props: {
+    nameId: [String, Number],
+    flexAuto: Boolean,
+    isButton: Boolean,
+    blockPadding: String,
+    textAlign: String,
+    textSize: String,
+    textWeight: String,
+  },
+  emits: ['clickComponentEvent'],
+  name: 'BlockWithData',
+  methods: {
+    clickEvent(event) {
+      let data = {"id": this.nameId, "value":event.target.innerText}
+      return this.$emit('clickComponentEvent', data)
+    }
+  },
+  computed: {
+    blockPaddingValue() {
+      return this.blockPadding || this.defaultBlockPadding
+    },
+    textFontSize() {
+      return this.textSize || this.defaultFontSize
+    },
+    textFontWeight() {
+      return this.textWeight || this.defaultFontWeight
+    },
+    eventHandler() {
+      return this.isButton ? this.clickEvent: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.key-container {
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  gap: 10px;
+  flex-direction: column;
+  background: rgba(0, 0, 0, 0.30);
+}
+</style>
